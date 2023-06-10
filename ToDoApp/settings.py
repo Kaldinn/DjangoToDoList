061da1080608011f0e283828a733add3af2e7c99
Kaladinn <danielskulimowski.biuro@gmail.com>
2023-06-10 20:45:30 +0200

    fix static folders

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -117,10 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/tasks/static/'
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "tasks/static",
+# ]
