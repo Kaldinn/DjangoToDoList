862f68be866769fc4b625680e59e64795acb66b2
Kaladinn <danielskulimowski.biuro@gmail.com>
2023-06-11 18:03:48 +0200

    try to add auth system to ToDoList

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 #URL Config
 urlpatterns = [
-    path('', views.index, name='list'),
+    path('tasks/', views.task_page, name='list'),
     path('update_task/<str:pk>/', views.update_task, name='update_task'),
-    path('delete/<str:pk>/', views.delete_task, name='delete')
+    path('delete/<str:pk>/', views.delete_task, name='delete'),
+    path('', views.login_page, name='login'),
+    path('register/', views.register_page, name='register')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm
 from .models import *
 from .forms import *
 
 # Create your views here.
 
-def index(request):
+def login_page(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('task_page')
+        else:
+            messages.info(request, "Username or password are inncorect")
+
+    return render(request, 'tasks/login.html')
+
+def register_page(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, f"Account was created for {user}")
+
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'tasks/register.html', context)
+
+def task_page(request):
     tasks = Task.objects.all()          #Pobiera wszystkie obiekty Task z bazy danych
     
     form = TaskForm()          
@@ -53,3 +89,7 @@ def delete_task(request, pk):
     }
 
     return render(request, 'tasks/delete.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
